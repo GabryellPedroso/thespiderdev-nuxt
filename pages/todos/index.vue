4d6ce43d10ea989e3992d6fd62629afc8090828c
Spider <gabryellcorrea.dev@gmail.com>
2020-05-08 03:46:48 -0300

    checkpoint

---
+++ pages/todos/index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  h1 {{ todos }}
+</template>
+
+<script>
+export default {
+  nama: 'todos',
+  
+  data() {
+    return {
+      todos: []
+    }
+  },
+
+  mounted() {
+    this.$axios.get('/todos').then((todos) => {
+      this.todos = todos
+    })
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
